50bb0d795d14c95a9c8d238c79ea6a56404309af
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-05-01 07:36:56 -0400

    Add files via upload

---
+++ project12-01.js
@@ -0,0 +1,25 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 12
+      Project 12-01
+
+      Project to display a dropdown menu
+      Author: 
+      Date:   
+
+      Filename: project12-01.js
+*/
+$ ( () => {
+	$("li.submenu").mouseover( e=> {
+		$(e.currentTarget).children("ul").show();
+	})
+	.mouseout( e=> {
+		$(e.currentTarget).children("ul").hide();
+	});
+});
+
+
+
+
+
+                                                
